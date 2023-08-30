e73ba18f77143c80a800650284b6838feb785839
Matt Litherland <mattsince87@gmail.com>
2023-08-30 12:36:02 +0300

    Fixed prisma schema issue for OAuthentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from 'next-auth/providers/github'
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { Adapter } from "next-auth/adapters";
-// import prisma from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
+import prisma from "@/lib/prisma";
 
 export const authOptions:NextAuthOptions = {
-  // adapter: PrismaAdapter(prisma) as Adapter,
+  adapter: PrismaAdapter(prisma) as Adapter,
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
