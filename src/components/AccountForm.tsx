5a85f6f0a51b578bfe4f4e34c7f1a5fb88d04a7e
Matt Litherland <mattsince87@gmail.com>
2023-08-31 10:57:06 +0300

    Added form to update profile

--- src/components/AccountForm.tsx
+++ src/components/AccountForm.tsx
@@ -1,37 +1,65 @@
 "use client";
 
-// import { signOut } from "next-auth/react";
-
+import { useState } from "react";
 interface Props {
   data: any;
 }
 
 export const AccountForm = ({ data }: Props) => {
-  // const handleDelete = async () => {
-  //   try {
-  //     const response = await fetch(`http://localhost:3000/api/user/${email}`, {
-  //       method: "DELETE",
-  //     });
-  //     if (response.ok) {
-  //       console.log("Account deleted");
-  //       signOut();
-  //     }
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // };
+  const [name, setName] = useState(data.user.name || "");
+  const [jobTitle, setJobTitle] = useState("");
+
+  const updateUser = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      console.log(name, jobTitle);
+      console.log(
+        "Fetch URL:",
+        `http://localhost:3000/api/user/${data.user.email}`
+      );
+      console.log("Sending request body: ", JSON.stringify({ name, jobTitle }));
+      const response = await fetch(
+        `http://localhost:3000/api/user/${data.user.email}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, jobTitle }), // Addint jobTitle breaks the request
+        }
+      );
+      console.log("HTTP Response: ", response);
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Account updated");
+        console.log("Server says: ", result);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
-    <div>
+    <form onSubmit={updateUser}>
       <div>
-        <input type="text" placeholder={data.user.name} />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder={data.user.name}
+        />
       </div>
       <div>
-        <input type="email" readOnly placeholder={data.user.email} />
+        <input
+          type="text"
+          value={jobTitle}
+          onChange={(e) => setJobTitle(e.target.value)}
+          placeholder="Job title"
+        />
       </div>
       <div>
-        <input type="text" placeholder="Job title" />
+        <button type="submit">Submit</button>
       </div>
-    </div>
+    </form>
   );
 };
