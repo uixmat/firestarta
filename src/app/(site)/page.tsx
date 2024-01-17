52919247400870508d990ba58e01cf0be9aa5555
Matt Litherland <mattsince87@gmail.com>
2024-01-17 13:43:25 +0200

    added core components directory for heroes, sections etc

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,9 @@
+import Hero from "@/components/core/hero/Hero";
+
 export default function Home() {
   return (
     <>
+      <Hero />
       <p>Hello</p>
     </>
   );
---
+++ src/components/core/hero/Hero.tsx
@@ -0,0 +1,17 @@
+// import styles from "./Hero.module.scss";
+
+export const Hero = () => {
+  return (
+    <div className="w-full py-64">
+      <div className="flex items-center justify-center flex-col">
+        <h1 className="font-bold text-6xl mb-4">Build better, build faster</h1>
+        <p>
+          A Next.js boilerplate to start your next project, with user
+          authentication, theming and more.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
