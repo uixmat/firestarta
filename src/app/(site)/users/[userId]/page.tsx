36ba58215b2db7cfd8863e87f88d735ee5be37d4
Matt Litherland <mattsince87@gmail.com>
2024-01-17 17:48:44 +0200

    Styled up basic profile and removed user component

--- src/app/(site)/users/[userId]/page.tsx
+++ src/app/(site)/users/[userId]/page.tsx
@@ -1,5 +1,7 @@
-import { getUserById, getUsers } from "@/lib/prisma/users";
-import User from "./user";
+import { getUserById } from "@/lib/prisma/users";
+import Hero from "@/components/core/hero/Hero";
+import { Section } from "@/components/core/section/Section";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 interface PageProps {
   params: {
@@ -19,7 +21,33 @@ async function getUser(userId: string) {
 const Page = async ({ params }: PageProps) => {
   const user = await getUser(params.userId);
 
-  return <User user={user} />;
+  return (
+    <>
+      <Hero
+        size="sm"
+        title={user.name as string}
+        description={user?.jobTitle || "Unemployed"}
+      />
+      <div className="container pb-20 max-w-lg">
+        <Section>
+          <Card>
+            <CardHeader>
+              <h2 className="text-2xl font-bold">{user.name}&apos;s profile</h2>
+              <p className="text-gray-500">User details and settings.</p>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium quam aliquam, sit veritatis sint sed corrupti ullam
+                aperiam enim quia blanditiis incidunt ex sapiente nulla velit
+                iste atque explicabo animi?
+              </p>
+            </CardContent>
+          </Card>
+        </Section>
+      </div>
+    </>
+  );
 };
 
 export default Page;
