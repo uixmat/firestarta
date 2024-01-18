aef7e24070a5ed3711dfc5d095c0ecc2e3188256
Matt Litherland <mattsince87@gmail.com>
2024-01-18 20:14:23 +0200

    updated button, removing brand text

--- src/components/core/header/GithubButton.tsx
+++ src/components/core/header/GithubButton.tsx
@@ -6,7 +6,7 @@ import { Github } from "lucide-react";
 export const GithubButton = () => {
   return (
     <Button size="sm" className="ml-auto" onClick={() => signIn()}>
-      <Github className="h-4" /> Sign in with Github
+      <Github className="h-4" /> Sign in
     </Button>
   );
 };
