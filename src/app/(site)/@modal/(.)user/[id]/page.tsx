d66662f35333b6d1dae3d06cee505b0243a1cefd
Matt Litherland <mattsince87@gmail.com>
2024-01-19 15:30:27 +0200

    fetching user data on modal

--- src/app/(site)/@modal/(.)user/[id]/page.tsx
+++ src/app/(site)/@modal/(.)user/[id]/page.tsx
@@ -1,20 +1,47 @@
+import { getUserById } from "@/lib/prisma/users";
+
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 
-export default function User() {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+async function getUser(id: string) {
+  const { user } = await getUserById(id);
+  if (!user) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return user;
+}
+
+const User = async ({ params }: PageProps) => {
+  const user = await getUser(params.id);
   return (
     <Dialog open>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Username</DialogTitle>
-          <DialogDescription>Text</DialogDescription>
+          <DialogTitle>{user.name}</DialogTitle>
+          <DialogDescription>{user.email}</DialogDescription>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            quam aliquam, sit veritatis sint sed corrupti ullam aperiam enim
+            quia blanditiis incidunt ex sapiente nulla velit iste atque
+            explicabo animi?
+          </p>
         </DialogHeader>
       </DialogContent>
     </Dialog>
   );
-}
+};
+
+export default User;
