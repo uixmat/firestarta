289f162fe751cd451c00f521f01b82f8a0e17a6b
Matt Litherland <mattsince87@gmail.com>
2024-01-23 15:04:49 +0200

    updated authOptions imports

--- src/app/(site)/account/page.tsx
+++ src/app/(site)/account/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { DeleteAccountButton } from "./DeleteAccountButton";
--- src/components/ServerSession.tsx
+++ src/components/ServerSession.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
 export default async function ServerSession() {
