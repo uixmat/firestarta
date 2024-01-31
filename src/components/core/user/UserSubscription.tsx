14f89264dfb5cc5e978d1e2452eab97036962dce
Matt Litherland <mattsince87@gmail.com>
2024-01-31 19:58:58 +0200

    improved styles, updated seo images/icons

--- src/components/core/user/UserSubscription.tsx
+++ src/components/core/user/UserSubscription.tsx
@@ -13,17 +13,17 @@ export default async function UserSubscription({ session }: UserProps) {
   return (
     <>
       <div>
-        <h3>{session?.user?.name}</h3>
-        <p>{session?.user?.jobTitle}</p>
+        <h3 className="mb-1 text-white">{session?.user?.name}</h3>
+        <p className="font-bold">{session?.user?.jobTitle}</p>
       </div>
       <div>
-        <h3>Subscription</h3>
+        <h3 className="mb-1 text-white">Subscription</h3>
         {user?.subscriptions && user.subscriptions.length > 0 ? (
           <>
             {user.subscriptions.map((subscription) => (
               <p key={subscription.id}>
-                <span className="inline-flex items-center">
-                  <BadgeCheck className="inline w-5 h-5 mr-2 text-lime-300" />{" "}
+                <span className="inline-flex items-center text-sm font-bold">
+                  <BadgeCheck className="inline w-5 h-5 mr-2 text-indigo-500" />{" "}
                   {subscription.name}
                 </span>
               </p>
