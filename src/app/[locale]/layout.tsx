a683fd79ed59d8199b3b13d7a7ae7509e0c3ba13
Matt Litherland <mattsince87@gmail.com>
2024-02-07 15:26:55 +0200

    removed params from layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,31 +1,23 @@
 import { NextAuthProvider } from "@/components/core/provider/NextAuthProvider";
 import { Toaster } from "@/components/ui/sonner";
 import { poppins } from "@/lib/utils/fonts";
-import type { Metadata } from "next";
 
 import "@/styles/globals.css";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL),
-  title: "Firestarta.dev - Your Next.js bootstrap",
-  description:
-    "Firestarta.dev is a Next.js bootstrap for your future projects with Authentication, Prisma and Postgres.",
-};
 
 interface LocaleLayoutProps {
   children: React.ReactNode;
   modal: React.ReactNode;
-  params: { locale: string };
+  // params: { locale: string };
 }
 
 export default function LocaleLayout({
   children,
-  params,
+  // params,
   modal,
 }: LocaleLayoutProps) {
   return (
     <html
-      lang={params.locale}
+      // lang={params.locale}
       className={`${poppins.variable} dark`}
       suppressHydrationWarning
     >
