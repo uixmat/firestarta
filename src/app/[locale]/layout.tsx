1513d60028ad6cc28c85a3f7c99c9382e87e3910
Matt Litherland <mattsince87@gmail.com>
2024-02-08 19:27:54 +0200

    test build

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,27 +4,41 @@ import { poppins } from "@/lib/utils/fonts";
 
 import "@/styles/globals.css";
 
-type LayoutProps = {
-  params: { locale: string };
-  modal: React.ReactNode;
+export default function LocaleLayout({
+  children,
+  params: { locale },
+}: {
   children: React.ReactNode;
-};
-
-export default function RootLayout({ params, children, modal }: LayoutProps) {
-  const { locale } = params;
+  params: { locale: string };
+}) {
   return (
-    <html
-      lang={locale}
-      className={`${poppins.variable} dark`}
-      suppressHydrationWarning
-    >
-      <body className="min-h-screen font-sans antialiased bg-background dark">
-        <NextAuthProvider>
-          {children}
-          {modal}
-          <Toaster position="top-center" richColors />
-        </NextAuthProvider>
-      </body>
+    <html lang={locale}>
+      <body>{children}</body>
     </html>
   );
 }
+
+// type LayoutProps = {
+//   params: { locale: string };
+//   modal: React.ReactNode;
+//   children: React.ReactNode;
+// };
+
+// export default function RootLayout({ params, children, modal }: LayoutProps) {
+//   const { locale } = params;
+//   return (
+//     <html
+//       lang={locale}
+//       className={`${poppins.variable} dark`}
+//       suppressHydrationWarning
+//     >
+//       <body className="min-h-screen font-sans antialiased bg-background dark">
+//         <NextAuthProvider>
+//           {children}
+//           {modal}
+//           <Toaster position="top-center" richColors />
+//         </NextAuthProvider>
+//       </body>
+//     </html>
+//   );
+// }
