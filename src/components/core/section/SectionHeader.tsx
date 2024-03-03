510a229c050e47d555b6b2aa3a533c08b024be8e
Matt Litherland <mattsince87@gmail.com>
2024-03-04 00:04:19 +0200

    improved leading on section titles

--- src/components/core/section/SectionHeader.tsx
+++ src/components/core/section/SectionHeader.tsx
@@ -12,7 +12,7 @@ export const SectionHeader = ({
   return (
     <header className="flex flex-col w-full gap-3 mb-12">
       <div className="relative flex flex-col items-center justify-center text-center text-transparent bg-clip-text bg-gradient-to-b to-blue-200 from-white">
-        <h2 className="mb-4 text-2xl font-bold md:text-5xl text-balance">
+        <h2 className="mb-4 text-2xl font-bold leading-snug md:text-5xl text-balance md:leading-snug">
           {title}
         </h2>
         <>{description}</>
